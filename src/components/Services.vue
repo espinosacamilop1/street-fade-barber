5f115779265e90120d3952f7c654112766850078
espinosacamilop1 <espinosacamilop1@gmail.com>
2024-11-01 01:12:08 -0400

    hero styles and starting services

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -5,5 +5,10 @@
 </script>
 
 <template>
-Servicios
+    <div class="services-container">
+
+        <h2>Nuestros Servicios</h2>
+        <p>En nuestro local ofrecemos una amplia gama de servicios para realzar tu estilo. Desde cortes de cabello para mujeres y hombres, hasta el cuidado de uñas y retoques de barba. Sea lo que sea que necesites, lo encontrarás con nosotros, brindando siempre un toque de calidad y estilo.</p>
+
+    </div>
 </template>

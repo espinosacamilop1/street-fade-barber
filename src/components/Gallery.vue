a042ab4c3f095f06f81918d00be037b7c38d45de
espinosacamilop1 <espinosacamilop1@gmail.com>
2024-11-03 14:19:05 -0500

    gallery section completed

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,7 +5,18 @@
 </script>
 
 <template>
-    <div>
-        Galeria
+    <div class="gallery-container section-container">
+        <h2>
+            Galeria
+        </h2>
+
+        <div class="gallery-section">
+            <button class="arrow left"></button>
+            
+            <img src="../assets/img/galery-placeholder.webp" alt="">
+
+            <button class="arrow right"></button>
+
+        </div>
     </div>
 </template>

b73f4fa99eaa6e80b307c58e3a597ebc38901ec8
espinosacamilop1 <espinosacamilop1@gmail.com>
2024-11-03 13:24:37 -0500

    services section completed

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -6,9 +6,49 @@
 
 <template>
     <div class="services-container">
+        <div class="services-header">
+            <h2>Nuestros Servicios</h2>
+    
+            <p>En nuestro local ofrecemos una amplia gama de servicios para realzar tu estilo. 
+                Desde cortes de cabello para mujeres y hombres, hasta el cuidado de uñas y retoques 
+                de barba. Sea lo que sea que necesites, lo encontrarás con nosotros, brindando 
+                siempre un toque de calidad y estilo.</p>
+        </div>
 
-        <h2>Nuestros Servicios</h2>
-        <p>En nuestro local ofrecemos una amplia gama de servicios para realzar tu estilo. Desde cortes de cabello para mujeres y hombres, hasta el cuidado de uñas y retoques de barba. Sea lo que sea que necesites, lo encontrarás con nosotros, brindando siempre un toque de calidad y estilo.</p>
+        <div class="services-list-wrapper">
+                <section class="services-list-background"></section>
+                
+                <section class="services-list">
+                    <div class="services-list-items services-list-one">
+                        <span class="services-list-item">
+                            <h2>Barberia</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla 
+                                velit nec diam fermentum, non vestibulum felis varius.</p>
+                        </span>
+                        
+                        <span class="services-list-item">
+                            <h2>Manicure</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla 
+                                velit nec diam fermentum, non vestibulum felis varius.</p>
+                        </span>
+                    </div>
+
+                    <div class="services-list-items services-list-two">
+                        <span class="services-list-item">
+                            <h2>Facial</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla 
+                                velit nec diam fermentum, non vestibulum felis varius.</p>
+                        </span>
+                        
+                        <span class="services-list-item">
+                            <h2>Peluqueria</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla 
+                                velit nec diam fermentum, non vestibulum felis varius.</p>
+                        </span>
+                    </div>
+                </section>
+
+        </div>
 
     </div>
 </template>

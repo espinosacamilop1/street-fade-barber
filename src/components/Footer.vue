339d03074bec0d30c7f68cf4a7009519421fb828
espinosacamilop1 <espinosacamilop1@gmail.com>
2024-11-03 14:42:59 -0500

    footer complete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,22 @@
 </script>
 
 <template>
-    <div>
-        Footer
+    <div class="footer section-container">
+        <section class="footer-sections contact-section">
+            <h2 class="white">Contactenos</h2>
+            <p class="heading-text white">123-456-7890</p>
+        </section>
+
+        <section class="footer-sections hours-section">
+            <h2 class="white">Horario</h2>
+            <p class="heading-text white">Mon-fri: 8am - 7pm </p>
+            <p class="heading-text white">Sat: 8am - 6pm </p>
+            <p class="heading-text white">Sun: 8am - 3pm </p>
+        </section>
+
+        <section class="footer-sections address-section">
+            <h2 class="white">Direccion</h2>
+            <p class="heading-text white">4244 E 4th Ave Hialeah, FL 33013</p>
+        </section>
     </div>
 </template>
